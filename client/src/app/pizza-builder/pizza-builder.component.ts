51afb837ea3036c79f5eab2f1768e4f5cc2bc512
teamgidi <b9611345@urhen.com>
2019-08-07 22:46:54 +0300

    :(

--- client/src/app/pizza-builder/pizza-builder.component.ts
+++ client/src/app/pizza-builder/pizza-builder.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { personInOrder, PizzaBuilderService } from './pizza-builder.service';
+ 
 @Component({
   selector: 'app-pizza-builder',
   templateUrl: './pizza-builder.component.html',
   styleUrls: ['./pizza-builder.component.less']
 })
 export class PizzaBuilderComponent implements OnInit {
+Arr = Array;
+  constructor(public pizzaBuilderService: PizzaBuilderService) { }
 
-  constructor() { }
-z
   ngOnInit() {
   }
 
+
 }
+
